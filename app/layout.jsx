44bd1f0f9b2a0eb7cdc642576fa840456ff3ede6
Daniel Kings <181596253+danielkingsk@users.noreply.github.com>
2025-05-14 17:04:25 -0400

    Creation of Main UI Components and Layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,28 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
-}
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
-export default layout
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
